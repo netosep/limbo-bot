992cc0302126dc39835ae216658ff25d800c279e
netosep <clementesepulveda27@gmail.com>
2021-08-20 20:58:43 -0300

    initial add command eval

---
+++ src/commands/eval.js
@@ -0,0 +1,27 @@
+
+
+module.exports = { 
+
+    help: {
+        name: "eval",
+        aliases: ["js"]
+    },
+
+    run: async (bot, message, args) => {
+        
+/*         var input = args.join(" ");
+        var output;
+
+        console.log(message.authort)
+
+        try {
+            output = eval(input)
+        } catch (err) {
+            console.error(err)
+        }
+
+        return message.channel.send(`\`\`\`js\n${output}\`\`\`` )*/
+
+    } 
+    
+}
