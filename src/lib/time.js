3c96eada837265242e303f032b51a83fb41ab34c
netosep <clementesepulveda27@gmail.com>
2021-08-26 08:16:16 -0300

    add new functions

---
+++ src/lib/time.js
@@ -0,0 +1,36 @@
+function msToSec(time) {
+    return Math.round(time / 1000);
+}
+
+function msToMin(time) {
+    return Math.round(time / 60000);
+}
+
+function msToHour(time) {
+    return Math.round(time / 3600000);
+}
+
+const convertTime = function(time) {
+
+    time = parseInt(time);
+
+    // segundos
+    if(time >= 1000 && time < 60000) { 
+        return `${msToSec(time)} segundos`;
+    }
+    // minutos
+    else if(time >= 60000 && time < 3600000) {
+        return `${msToMin(time)} minuto${msToMin(time) > 1 ? "s" : ""}`;
+    }
+    // horas
+    else if(time >= 3600000) {
+        var hour = msToHour(time);
+        //var min = time % 3600000
+        return `${hour} hora${hour > 1 ? "s" : ""}`
+    } else {
+        return `${time} ms`;
+    }
+
+}
+
+module.exports = { convertTime }
