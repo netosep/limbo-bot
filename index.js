f97f0d36ee259fad0751d69dbbb58f1a218c9eb7
netosep <clementesepulveda27@gmail.com>
2021-08-26 08:07:25 -0300

    remove .env configs

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 const lib = require("./src/lib/functions");
-const env = require("dotenv");
-env.config();
-
-const token = process.env.TOKEN_BOT;
+const { token } = require('./config.json');
 
 bot.commands = new discord.Collection();
 bot.aliases = new discord.Collection();
