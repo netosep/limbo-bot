75b19743844ab211b3bf8252cd5f61e1fb586d4b
netosep <clementesepulveda27@gmail.com>
2021-08-26 08:15:10 -0300

    add presence status

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,18 @@ const database = require("../database/connection");
 
 bot.on("ready", async () => {
 
+    var status = [
+        "boas energias!"
+    ];
+
+    let i = 0;
+    await bot.user.setStatus('dnd') // dnd, idle, online, invisible
+    setInterval(() => bot.user.setActivity(`${status[i++ % status.length]}`, { 
+        type:"STREAMING", // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
+        //url: "https://www.twitch.tv/net0xy",
+        url: "https://www.youtube.com/watch?v=sSQH1Hin9-I", // video do samuel silva
+     }), 10000);
+
+    console.log("Connecting to the database...");
     database.testConnection()
 })
