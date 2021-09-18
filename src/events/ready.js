b1e6c8a1285914e70cdf62cae236793c0012a59b
netosep <clementesepulveda27@gmail.com>
2021-09-18 19:05:23 -0300

    remove old database connection test

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,19 @@
 const { bot } = require("../../index");
-const database = require("../database/connection");
 
 bot.on("ready", async () => {
 
-    database.testConnection()
+    var status = [
+        "boas energias!"
+    ];
+
+    let i = 0;
+    await bot.user.setStatus('dnd') // dnd, idle, online, invisible
+    setInterval(() => bot.user.setActivity(`${status[i++ % status.length]}`, { 
+        type:"STREAMING", // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
+        //url: "https://www.twitch.tv/net0xy",
+        url: "https://www.youtube.com/watch?v=sSQH1Hin9-I", // video do samuel silva
+     }), 10000);
+
+    console.log("Connecting to the database...");
+
 })
