843015b96a6458fc39196583a2d8ce58e07a5938
netosep <clementesepulveda27@gmail.com>
2021-09-18 18:47:41 -0300

    create new command `filter`

---
+++ src/commands/filter.js
@@ -0,0 +1,33 @@
+
+module.exports = { 
+
+    help: {
+        name: "filter",
+        aliases: ["fil", "effect"]
+    },
+
+    run: async (bot, message, args) => {
+
+        if(message.author.id != "232310431174230026") return;
+
+        if(!message.member.voice.channel) {
+            message.react("❎");
+            return message.channel.send(`> **${message.author}, você precisa estar em um canal pra poder executar esse comando...  😕**`);
+        }
+
+        if(bot.distube.isPlaying(message)) {
+            let filter = args[0];
+
+            if(!filter) {
+                return message.channel.send("filtros: \`3d\`, \`bassboost\`, \`echo\`, \`karaoke\`, \`nightcore\`, \`vaporwave\`, \`flanger\`, \`gate\`, \`haas\`, \`reverse\`, \`surround\`, \`mcompand\`, \`phaser\`, \`tremolo\`, \`earwax\`")
+            }
+            
+            bot.distube.setFilter(message, filter);
+        } else {
+            message.react("❎");
+            return message.channel.send("> **Que eu saiba, não estou tocando nada nesse servidor...  🙄**");
+        }
+    
+    } 
+    
+}
