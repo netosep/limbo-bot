f6d7918434c5fe496b4731a32dff91c5fd628e88
netosep <clementesepulveda27@gmail.com>
2021-09-18 18:46:34 -0300

    command `avatar` send a Embed

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,17 +1,30 @@
+const { MessageEmbed } = require("discord.js");
 
 module.exports = { 
 
     help: {
         name: "avatar",
+        usage: [`avatar`, `avatar <@user>`],
+        description: "mostra o seu avatar ou o de alguém mencionado.",
+        accessableBy: 'Todos os membros.',
         aliases: ["a", "perfil"]
     },
 
     run: async (bot, message, args) => {
 
         var target = message.mentions.users.first() || message.author;
+        var embed = new MessageEmbed();
+        var time = new Date();
 
+        message.channel.send(
+            embed.setColor("BLACK")
+            .setDescription(`> **Avatar de ${target}**
+                             > [Clique aqui](${target.displayAvatarURL({ size: 4096, dynamic: true })}) para baixar`)
+            .setImage(target.displayAvatarURL({ size: 4096, dynamic: true }))
+            .setFooter(`© ${bot.user.username} - ${time.getFullYear()}`, bot.user.displayAvatarURL())
+            .setTimestamp()
+        );
 
-        message.channel.send(target.displayAvatarURL({ size: 4096, dynamic: true }))
     } 
     
 }
