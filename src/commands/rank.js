977dec70667d56573d9c8c84f1681eb369d6b02b
netosep <clementesepulveda27@gmail.com>
2021-09-18 18:49:37 -0300

    disable command `rank`

--- src/commands/rank.js
+++ src/commands/rank.js
@@ -1,4 +1,7 @@
-const mysql = require("../database/connection");
+const canvacord = require("canvacord");
+const { MessageAttachment } = require("discord.js");
+//const { convertTime } = require("../lib/time");
+const Rank = require("../utils/rankQuery");
 
 module.exports = { 
 
@@ -9,32 +12,42 @@ module.exports = {
 
     run: async (bot, message, args) => {
 
-        var user = message.mentions.users.first() || message.author;
-
-        //console.log(user)
+/*         var user = message.mentions.users.first() || message.author;
 
         if(user.bot) return;
 
-        mysql.execute(`SELECT * FROM user WHERE user_id = ?`, [ user.id ])
-        .then((result) => {
+        await Rank.findRankByIds(user.id, message.guild.id)
+        .then((userRank) => {
+            if(userRank.id) {
+                var totalExp = userRank.xp;
+                var totalExpForNxtLVL = userRank.nxt_lvl_xp;
+                var levelExp = Math.round(totalExpForNxtLVL - (totalExpForNxtLVL / 2.5)); 
+                var nowExp = Math.round(totalExp - (totalExpForNxtLVL / 2.5));
+                //var time = convertTime(userRank.time_connected);
 
-            if(result.length === 0) {
-                return message.channel.send('sem resultados')
+                const rank = new canvacord.Rank()
+                    .setAvatar(user.displayAvatarURL({ size: 1024, format: "jpg" }))
+                    .setUsername(user.username)
+                    .setDiscriminator(user.discriminator)
+                    .setStatus(user.presence.status, true)
+                    .setRequiredXP(levelExp)
+                    .setCurrentXP(nowExp)
+                    .setLevel(userRank.level)
+                    .setRank(0, "", false)
+                    
+                rank.build().then((data) => {
+                    const attachment = new MessageAttachment(data, `rankcard-${user.username}.png`);
+                    message.channel.send(attachment)
+                });
+
+            } else {
+                message.channel.send("sem resultados")
             }
-
-            return message.channel.send(`\`\`\`fix\n`+
-                `ID: ${result[0].user_id}\n`+
-                `Nome: ${result[0].user_name}\n`+
-                `XP: ${result[0].experience}\n`+
-                `Level: ${result[0].level}\n`+
-            `\`\`\``);
-
         })
         .catch((err) => {
-            console.error(err)
-        });
+            return console.error(err);
+        }); */
 
-        //message.channel.send(message.author.id)
     } 
     
 }
