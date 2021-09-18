e44f09aeb87f8614559cebb926e30cf71c64b1a8
netosep <clementesepulveda27@gmail.com>
2021-09-18 18:50:08 -0300

    create new command `skip`

---
+++ src/commands/skip.js
@@ -0,0 +1,32 @@
+
+module.exports = { 
+
+    help: {
+        name: "skip",
+        aliases: ["s", "next", "fs"]
+    },
+
+    run: async (bot, message, args) => {
+
+        if(!message.member.voice.channel) {
+            message.react("❎");
+            return message.channel.send(`
+                > **Foi mal ai ${message.author} 😞
+                > Você precisa estar em um canal pra poder executar esse comando...  😕**
+            `);
+        }
+
+        
+
+        if(bot.distube.isPlaying(message)) {
+            message.react("⏭");
+            message.channel.send(`> **Próxima música... ⏭**`);
+            bot.distube.skip(message);
+        } else {
+            message.react("❎");
+            message.channel.send("> **Que eu saiba, não estou tocando nada nesse servidor...  🙄**");
+        }
+
+    } 
+    
+}
