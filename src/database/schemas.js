f30343b55cdae4176e01fd9f6be0cbe35af8f11f
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 19:34:15 -0300

    create new schemas file

---
+++ src/database/schemas.js
@@ -0,0 +1,52 @@
+const { Schema } = require("mongoose");
+
+const guildInfoTable = new Schema({
+    guild_id       : { type: String, default: '0', required: true },
+    guild_name     : { type: String, default: '0', required: true },
+    guild_owner_id : { type: String, default: '0', required: true },
+    guild_icon_url : { type: String, default: '0', required: true },
+    guild_prefix   : { type: String, default: '?', required: true },
+    enable_rank    : { type: Boolean, default: false, required: true },
+    created_at     : { type: Date, default: Date.now(), required: true }
+},
+{ 
+    versionKey : false
+});
+
+const userInfoTable = new Schema({
+    user_id         : { type: String, default: '0', required: true },
+    user_name       : { type: String, default: '0', required: true },
+    user_avatar_url : { type: String, default: '0', required: true },
+    created_at      : { type: Date, default: Date.now(), required: true }
+},
+{ 
+    versionKey : false
+});
+
+const rankInfoTable = new Schema({
+    user_id        : { type: String, default: '0', required: true },
+    guild_id       : { type: String, default: '0', required: true },
+    level          : { type: Number, default: 0, required: true },
+    experience     : { type: Number, default: 0, required: true },
+    next_level_exp : { type: Number, default: 100, required: true },
+    messages       : { type: Number, default: 0, required: true },
+    time_connected : { type: String, default: '0', required: true }
+},
+{ 
+    versionKey : false
+});
+
+const disabledChannelsTable = new Schema({
+    guild_id   : { type: String, default: '0', required: true },
+    channel_id : { type: String, default: '0', required: true }
+},
+{ 
+    versionKey : false
+});
+
+module.exports = {
+    guildInfoTable,
+    userInfoTable,
+    rankInfoTable,
+    disabledChannelsTable
+}
