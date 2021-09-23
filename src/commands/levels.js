5153bbeebe3168a96c34be144cf82cf267e2e41e
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 16:04:56 -0300

    disable command levels

--- src/commands/levels.js
+++ src/commands/levels.js
@@ -1,5 +1,6 @@
-const { convertTime } = require("../lib/time");
-const Rank = require("../utils/rankQuery");
+const env = require("dotenv");
+
+env.config();
 
 module.exports = { 
 
@@ -10,26 +11,7 @@ module.exports = {
 
     run: async (bot, message, args) => {
 
-        if(message.author.id != "232310431174230026") return;
-
-        var string = "```fix\nNOME - NIVEL - XP - MENSAGENS - TEMPO CONECTADO\n";
-
-        await Rank.findAllRanks()
-        .then((ranks) => {
-            //console.log(ranks)
-
-            ranks.forEach(rank => {
-                string += `${rank.name} - ${rank.level} - ${rank.xp} - ${rank.messages} - ${convertTime(rank.time_connected)}\n`
-            });
-
-            string += "```"
-
-            message.channel.send(string)
- 
-        })
-        .catch((err) => {
-            return console.error(err);
-        });
+       /*  message.channel.send(`${process.env.LEVELS_URL}/guild/${message.guild.id}`) */
 
     } 
     
