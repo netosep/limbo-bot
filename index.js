032f549e4a1ab5e7ba5ec6504a6da8b5d2d5d7df
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 16:37:38 -0300

    create new musicPlayer `DisTube`

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const discord = require('discord.js');
+const DisTube = require("distube");
+const env = require("dotenv");
 const bot = new discord.Client();
 const lib = require("./src/lib/functions");
-const { token } = require('./config.json');
+
+env.config();
 
 bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
+bot.aliases  = new discord.Collection();
+bot.distube  = new DisTube(bot, { searchSongs: false, emitNewSongOnly: true });
+bot.database = require("./src/database/mongodb");
+
 lib.setup(bot);
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN);
 
 module.exports = { bot }
