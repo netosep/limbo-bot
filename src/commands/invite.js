e5fa8f90d3dedc436927ca3cb73274664a150fc4
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 00:05:05 -0300

    create new command `invite`

---
+++ src/commands/invite.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+
+    help: {
+        name: "invite",
+        usage: ["invite"],
+        description: "te envia o link de convite do bot.",
+        accessableby: "todos os membros.",
+        aliases: ["convite", "link"]
+    },
+    
+    run: async (bot, message, args) => {
+        
+        let embed = new MessageEmbed().setColor("BLACK");
+        let time = new Date();
+        // link de convite do bot
+        let link = `https://discord.com/oauth2/authorize?=&client_id=${bot.user.id}&scope=bot&permissions=8`
+
+        message.react('🤙🏿').catch(() => { return });
+        
+        message.author.send(embed
+            .setAuthor(`Olá ${message.author.username}!`, message.author.displayAvatarURL())
+            .setDescription(`> **[Clique aqui](${link}) para poder me adicionar ao seu servidor!**`)
+            .setFooter(`© ${bot.user.username} - ${time.getFullYear()} | Todos os direitos reservados.`, bot.user.displayAvatarURL())
+        ).catch(() => { return });
+    }
+}
