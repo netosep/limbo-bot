165c71232cd0c0780241a6e75863e4ce028a527d
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 19:33:26 -0300

    create new command `uptime`

---
+++ src/commands/misc/uptime.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+
+    help: {
+        name: "uptime",
+        usage: ["uptime"],
+        description: "Mostra o tempo online do bot.",
+        accessableby: "Todos os membros.",
+        aliases: ["upt"]
+    },
+    
+    run: async (bot, message, args) => {
+
+        let embed = new MessageEmbed().setColor("BLACK")
+        message.channel.send(embed
+            .setDescription(`**> Estou online a \`${ms(bot.uptime, {long: true})}\`**`)
+        ).catch(() => { return });
+
+    }
+}
