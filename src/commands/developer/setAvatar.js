0e1236f973964d5e809da58f5d55974605faa805
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-23 23:52:54 -0300

    refactor command `setAvatar`

---
+++ src/commands/developer/setAvatar.js
@@ -0,0 +1,31 @@
+
+module.exports = { 
+
+    help: {
+        name: "setavatar",
+        aliases: ["sa"]
+    },
+
+    run: async (bot, message, args) => {
+        var link = args.join(" ");
+
+        if(message.author.id != process.env.BOT_OWNER_ID) return;
+
+        if(link.startsWith("https://")){
+            //message.channel.send(link)
+            console.log(bot)
+            bot.user.setAvatar(link)
+            .then(() => {
+                console.log("avatar redefinido")
+            })
+            .catch((err) => {
+                console.error(errr)
+            })
+        } else {
+            message.channel.send("erro")
+        }
+
+        
+    } 
+    
+}
