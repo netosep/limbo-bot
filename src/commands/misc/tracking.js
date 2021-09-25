06bfdaa3e89cf60a29f7b90e47c1ffa42f6f68cb
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-25 02:05:04 -0300

    add new help attributes and update command

--- src/commands/misc/tracking.js
+++ src/commands/misc/tracking.js
@@ -1,46 +1,48 @@
 const { MessageEmbed } = require("discord.js")
-const axios = require('axios');
-const moment = require('moment');
+const axios = require("axios");
+const moment = require("moment");
 
 module.exports = {
 
     help: {
-        name: "rastreio",
+        name: "rastrear",
+        usage: ["rast <CODIGODERASTREIO>"],
+        description: "Obtem os dados de rastreio de um encomenda dos correios.",
+        accessableBy: "Todos os membros.",
         aliases: ["tracking", "rast"]
     },
 
     run: async (bot, message, args) => {
-        var tracking = args.join(" ");
 
-        if (tracking.length < 1 ) {
-            message.react("❎");
-            return message.channel.send("Mensagem inválida!")
-        }
+        let tracking = args.join(" ");
+        if (tracking.length < 1) return;
   
         await axios(`https://proxyapp.correios.com.br/v1/sro-rastro/${tracking}`)
         .then(({ data }) => {
 
             let objeto = data.objetos[0];
-            let eventos = objeto.eventos
+            let eventos = objeto.eventos;
             let embed = new MessageEmbed().setColor("BLACK");
 
             if(objeto.mensagem) {
-                // mensagem não existe
+                message.react("❎");
+                return message.channel.send("> **Código de rastreio inválido!** 😕");
             }
 
             for (let i = (eventos.length -1); i >= 0; i--) {
-                embed.addField(`• **${eventos[i].descricao}**`,
-                    `> 📍 ${eventos[i].unidade.tipo} - ${eventos[i].unidade.endereco.cidade}-${eventos[i].unidade.endereco.uf}
+                embed.addField(`• ${eventos[i].descricao}`,
+                    `> 📆 Atualizado em \`${moment(eventos[i].dtHrCriado).format("DD/MM/YYYY")}\`
+                     > 📍 ${eventos[i].unidade.tipo} - ${eventos[i].unidade.endereco.cidade}-${eventos[i].unidade.endereco.uf}
                     ${eventos[i].unidadeDestino ? "> ✈ " + eventos[i].unidadeDestino.tipo + " - " +
                       eventos[i].unidadeDestino.endereco.cidade + "-" + eventos[i].unidadeDestino.endereco.uf : ""}`
                 );
             }
 
             message.channel.send(embed
-                .setTitle(`**🧭 Rastreando objeto de ${message.author.username}**`)
+                .setTitle(`🧭 **Rastreando objeto pela API dos CORREIOS**`)
                 .setThumbnail("https://i.imgur.com/unNhvOp.png")
                 .setDescription(`> 🔗 Código de rastreio: **\`${objeto.codObjeto}\`**`)
-                .setFooter(`Previsão de chegada: ${moment(objeto.dtPrevista).format("DD/MM/YYYY")} 📅 `, bot.user.displayAvatarURL())
+                .setFooter(`Previsão de chegada: ${moment(objeto.dtPrevista).format("DD/MM/YYYY")} 📅`, bot.user.displayAvatarURL())
             );
 
         })
