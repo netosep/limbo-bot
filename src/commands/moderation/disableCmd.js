f9d2d74b3b2ebfa6d237f9c10dfd8362dc6a1514
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-09-27 04:24:18 -0300

    create new command `disableCmd`

---
+++ src/commands/moderation/disableCmd.js
@@ -0,0 +1,72 @@
+
+module.exports = { 
+
+    help: {
+        name: "disable",
+        usage: ["disable <command>", "dc <comand> <command>"],
+        description: "Desativa o uso do comando no canal.",
+        accessableBy: "Administrador.",
+        aliases: ["dc", "desativarcmd", "disablecmd"]
+    },
+
+    run: async (bot, message, args) => {
+
+        if(message.member.hasPermission("ADMINISTRATOR")) {
+            
+            let dataDB = await bot.database.disabledCmds.findOne(
+                { guild_id: message.guild.id, channel_id: message.channel.id }
+            );
+
+            let commandsToDisable = [];
+            args.forEach((arg) => {
+                var command = bot.commands.get(bot.aliases.get(arg.toLowerCase()) || arg.toLowerCase());
+                if(command) commandsToDisable.push(command.help.name);
+            });
+
+            if(dataDB) {
+                dataDB.commands.forEach((cmdDB) => {
+                    commandsToDisable.forEach((cmd) => {
+                        if(cmdDB === cmd) {
+                            var index = commandsToDisable.indexOf(cmd);
+                            commandsToDisable.splice(index, 1);
+                        }
+                    });
+                });
+
+                if(commandsToDisable.length > 0) {
+
+                    commandsToDisable.forEach((cmd) => { dataDB.commands.push(cmd) });
+
+                    await bot.database.disabledCmds.updateOne(
+                        { guild_id: message.guild.id, channel_id: message.channel.id },
+                        {
+                            guild_id   : message.guild.id,
+                            channel_id : message.channel.id,
+                            commands   : dataDB.commands
+                        }
+                    );
+
+                    let successString = "O comando foi desativado nesse canal";
+                    if(commandsToDisable.length > 1) successString = "Os comandos foram desativados nesse canal";
+                    return message.channel.send(`> **${successString}.**`);
+
+                } else {
+                    return message.channel.send("> **Esse comando já está desabilitado para esse canal!**");
+                }
+                
+            } else {
+
+                return await new bot.database.disabledCmds({
+                    guild_id   : message.guild.id,
+                    channel_id : message.channel.id,
+                    commands   : commandsToDisable
+                }).save();
+            }
+
+        } else {
+            return message.channel.send("> **Você não tem permissão para acessar esse comando!**");
+        }
+        
+    } 
+    
+}
