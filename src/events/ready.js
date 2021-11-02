b4a13896ca63e8077d75a1ed0e09ab9926c508fe
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-11-01 21:48:47 -0300

    update status array

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,21 +2,19 @@ const { bot } = require("../../index");
 
 bot.on("ready", async () => {
 
-    var status = [
+    let status = [
         "boas energias!",
         "use ?help",
-        "novos comandos!",
-        `em ${bot.guilds.cache.size} servidores 🥰`,
-        "me mencione para obter ajuda...",
-        `para ${bot.users.cache.size} usuários 🤪`
+        `em ${bot.guilds.cache.size} servidores! 🥰`,
+        "me mencione para obter ajuda..."
     ];
 
     let i = 0;
-    await bot.user.setStatus('dnd') // dnd, idle, online, invisible
+    bot.user.setStatus('dnd') // dnd, idle, online, invisible
     setInterval(() => bot.user.setActivity(`${status[i++ % status.length]}`, { 
         type:"STREAMING", // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
         //url: "https://www.twitch.tv/net0xy",
         url: "https://www.youtube.com/watch?v=sSQH1Hin9-I", // video do samuel silva
-     }), 10000);
+    }), 10000);
 
 });
