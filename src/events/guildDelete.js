89dab88a91d53de448d9f7ac9d31e03e22473619
Neto Sepulveda <clementesepulveda27@gmail.com>
2021-11-06 02:22:43 -0300

    remove deprecated code

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,10 +1,5 @@
 const { bot } = require("../../index");
-require("dotenv").config();
 
 bot.on("guildDelete", (guild) => {
-
-    let globalLogChannel = bot.channels.cache.get(process.env.LOG_CHANNEL_ID);
-    let owner = guild.owner.user;
-    if(globalLogChannel) globalLogChannel.send(`Saí do servidor: **${guild.name}** - Dono: **${owner.username}#${owner.discriminator}**`);
-
+    return console.log(`Fui removido do servidor: ${guild.name}`);
 });
