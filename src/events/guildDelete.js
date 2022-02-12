b06de73c3ecba4f4661b5d56ce5da7696cdac9aa
Neto Sepulveda <clementesepulveda27@gmail.com>
2022-02-11 22:20:12 -0300

    update event guildDelete

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,5 +1,8 @@
 const { bot } = require("../../index");
 
-bot.on("guildDelete", (guild) => {
+bot.on("guildDelete", async (guild) => {
+
+    await bot.database.guildInfo.updateOne({ guild_id: guild.id }, { active : false });
     return console.log(`Fui removido do servidor: ${guild.name}`);
+
 });
